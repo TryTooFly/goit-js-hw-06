801fccf7e3045493d64a7a100b05d7b741f40f90
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-07-09 20:54:28 +0300

    work

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,6 @@
+// Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", addValidationClass);
