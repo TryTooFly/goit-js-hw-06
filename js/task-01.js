534694cfed869d94719608a034c0f338559abdc8
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-07-09 20:48:44 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,8 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
 const listCountRef = document.querySelector("#categories");
 console.log(`Number of categories: ${listCountRef.children.length}`);
 
